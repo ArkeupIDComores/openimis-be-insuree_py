c991faa0312c2d457321fe6b65f94debfb5d1879
Iandraina-etech <a.rabarisoa@etechconsulting-mg.com>
2025-07-22 16:45:33 +0300

    Update schema.py

--- insuree/schema.py
+++ insuree/schema.py
@@ -521,4 +521,3 @@ def _get_contextual_insuree_officers(info, location_id=None, **kwargs):
 
         # No officers found → return all valid EOs
         return Officer.objects.filter(validity_to__isnull=True)
-        #space versus tabulation comparaison
